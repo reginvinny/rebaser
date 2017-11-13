6a5108a724664dbc0222a2a86f9daa95e29ae745
reginvinny <33618554+reginvinny@users.noreply.github.com>
2017-11-13 21:56:45 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,10 +20,10 @@ echo - Enter "rebaser" command to sync with master
 
 echo  Rebaser Un-installation steps
 
-echo  via CURL
-echo sh -c "$(curl -fsSL https://raw.githubusercontent.com/reginvinny/rebaser/master/uninstall.sh)"
+echo "via CURL"
+echo "sh -c "$(curl -fsSL https://raw.githubusercontent.com/reginvinny/rebaser/master/uninstall.sh)""
 
-echo via WGET
-echo sh -c "$(wget https://raw.githubusercontent.com/reginvinny/rebaser/master/uninstall.sh -O -)"
+echo "via WGET"
+echo "sh -c "$(wget https://raw.githubusercontent.com/reginvinny/rebaser/master/uninstall.sh -O -)""
 echo " "
 echo " "
