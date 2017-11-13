1a991990a308c74932297117359fcd1794eec317
Regin Vinny <regin.vinny@qantas.com.au>
2017-11-13 18:54:43 +1100

    install utils

---
+++ install_utils/rebase_branch.sh
@@ -0,0 +1,45 @@
+echo ' '
+echo '#######################################'
+echo '#        Welcome to Rebaser!          #'
+echo '#        Author: Regin Vinny          #'
+echo '#######################################'
+echo ''
+curr_branch=$(git rev-parse --abbrev-ref HEAD)
+echo 'Current branch : ' $curr_branch
+echo ''
+git checkout master
+echo ''
+git pull --rebase
+echo ' '
+read -r -p "Are you sure? [Y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+clear
+echo ''
+echo '******** Rebasing branch' $curr_branch 'with master ********'
+echo ' '
+git checkout $curr_branch
+echo ''
+git pull
+echo ''
+git rebase master
+clear
+echo ''
+echo '******** Force pushing changes ********'
+echo ' '
+git push --force
+clear
+echo ' '
+echo '******** Rebase with master complete! ********'
+echo ' '
+echo ' '
+else
+echo ' '
+echo ' '
+git checkout $curr_branch
+clear
+echo ' '
+echo '******** Rebase cancelled! ********'
+echo ' '
+echo ' '
+fi
