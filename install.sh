907181dc476df3735c622a07a5857bffb75b70bc
Regin Vinny <regin.vinny@qantas.com.au>
2017-11-13 22:01:34 +1100

    fix

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ git checkout install
 cd rebaser
 echo "alias rebaser='sh ~/rebaser/install_utils/rebaser_utlil.sh'" >> ~/.zshrc
 echo "alias rebaser='sh ~/rebaser/install_utils/rebaser_utlil.sh'" >> ~/.bash_profile
+echo "alias uninstall_rebaser='sh ~/rebaser/uninstall.sh'" >> ~/.zshrc
+echo "alias uninstall_rebaser='sh ~/rebaser/uninstall.sh'" >> ~/.bash_profile
 
 clear
 echo " "
@@ -19,11 +21,6 @@ echo - Checkout feature branch
 echo - Enter "rebaser" command to sync with master
 
 echo  Rebaser Un-installation steps
-
-echo "via CURL"
-echo "sh -c "$(curl -fsSL https://raw.githubusercontent.com/reginvinny/rebaser/master/uninstall.sh)""
-
-echo "via WGET"
-echo "sh -c "$(wget https://raw.githubusercontent.com/reginvinny/rebaser/master/uninstall.sh -O -)""
+echo - Use "uninstall_rebaser" command
 echo " "
 echo " "
