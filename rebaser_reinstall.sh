4562a8eae9d656ad94b75ae8494ff9ba1220215c
Regin Vinny <regin.vinny@qantas.com.au>
2017-11-14 10:29:54 +1100

    changes

---
+++ rebaser_reinstall.sh
@@ -0,0 +1,16 @@
+#---Rebaser Reinstall File---#
+cd ~
+uninstall_rebase
+echo " "
+echo " "
+sleep 1
+clear
+echo " "
+echo " "
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/reginvinny/rebaser/master/install.sh)"
+echo " "
+echo " "
+echo "---------------Rebaser Reinstall Complete---------------"
+echo " "
+echo " "
+
