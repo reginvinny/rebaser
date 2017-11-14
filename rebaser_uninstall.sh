737072081fa46b6e6ae6c1d9a5f29689f3ec33f4
Regin Vinny <regin.vinny@qantas.com.au>
2017-11-14 13:30:57 +1100

    more changes

--- rebaser_uninstall.sh
+++ rebaser_uninstall.sh
@@ -1,13 +1,8 @@
 #---Rebaser Uninstall File---#
 
-sed -i '' '/rebaser_util.sh/d' ~/.zshrc
-sed -i '' '/rebaser_util.sh/d' ~/.bash_profile
+sed -i '' '/ralias rebaser=/d' ~/.zshrc
+sed -i '' '/ralias rebaser=/d' ~/.bash_profile
 
-sed -i '' '/rebaser_uninstall.sh/d' ~/.zshrc
-sed -i '' '/rebaser_uninstall.sh/d' ~/.bash_profile
-
-sed -i '' '/rebaser_reinstall.sh/d' ~/.zshrc
-sed -i '' '/rebaser_reinstall.sh/d' ~/.bash_profile
 
 cd ~
 rm -rf rebaser
