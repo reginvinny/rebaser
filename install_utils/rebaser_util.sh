182bafd58aaf4daa96bb82ada806db1bab0f3e08
Regin Vinny <regin.vinny@qantas.com.au>
2018-01-05 17:24:36 +1100

    more changes

--- install_utils/rebaser_util.sh
+++ install_utils/rebaser_util.sh
@@ -8,17 +8,55 @@ curr_branch=$(git rev-parse --abbrev-ref HEAD)
 ref_branch=master
 echo 'Current branch : ' $curr_branch
 echo ''
+echo 'Select option (1 or 2)'
+echo ' 1. Rebase current branch with master branch'
+echo ' 2. Rebase current branch with another branch'
+echo ''
+echo 'Enter the option: ' && read option
+echo ''
+echo 'Selected option is ..... '$option
+echo ''
+if [[ "$option" == "1" ]]
+then
+ref_branch=master
+elif [[ "$option" == "2" ]]
+then
+echo ---------
+git branch
+echo ''
+echo 'Enter the reference branch name: ' && read ref_branch_name
+echo ''
+echo 'Selected branch name is '$ref_branch_name
+echo ''
+ref_branch=$ref_branch_name
+if git rev-parse --quiet --verify $ref_branch_name > /dev/null;
+then
+   echo "Branch name '$ref_branch_name' exists! Proceeding..."
+else 
+echo "Branch name '$ref_branch_name' doesn't exists! Exiting..."
+echo ' '
+echo ' '
+exit 1
+fi
+else
+echo ' '
+echo 'Error... Please try again '
+echo ' '
+exit 1
+fi
+echo '--------------------------------'
+read -r -p "Are you sure? [Y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+echo ''
 git checkout $ref_branch
 echo ''
 git pull --rebase
 echo ' '
-read -r -p "Are you sure? [Y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-then
 clear
 echo ''
 echo ' '
-echo '******** Rebasing branch' $curr_branch 'with $ref_branch ********'
+echo "******** Rebasing branch $curr_branch with $ref_branch ********"
 echo ' '
 git checkout $curr_branch
 echo ''
@@ -34,7 +72,7 @@ git push --force
 clear
 echo ' '
 echo ' '
-echo '******** Rebase with $ref_branch complete! ********'
+echo "******** Rebase with $ref_branch complete! ********"
 echo ' '
 echo ' '
 else
